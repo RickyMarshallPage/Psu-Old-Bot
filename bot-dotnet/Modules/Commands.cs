74161579684127b11fe0c206b1efbf3b0f255258
x86chk <x86chk@JASPER>
2022-01-09 13:02:21 -0500

    Add project files.

---
+++ bot-dotnet/Modules/Commands.cs
@@ -0,0 +1,146 @@
+﻿using Discord;
+using Discord.Commands;
+
+using System;
+using System.Linq;
+using System.IO;
+
+using System.Text;
+using System.Text.RegularExpressions;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using System.Collections;
+using System.Collections.Generic;
+
+using Discord.WebSocket;
+
+using Newtonsoft.Json;
+
+namespace DiscordBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        private ulong GuildID = 929751465285812224;
+
+        [Command("obfuscate")]
+        public async Task Obfuscate()
+        {
+            ulong UserID = Context.Message.Author.Id;
+
+            SocketGuildUser User = Program.Client.GetGuild(GuildID).GetUser(UserID);
+
+            bool License = false;
+            bool ServerBoost = false;
+
+            foreach (SocketRole Role in User.Roles)
+            {
+                if (Role.Name == "Buyer")
+                {
+                    License = true;
+                }
+                else if (Role.Name == "Server Booster")
+                {
+                    ServerBoost = true;
+                };
+            };
+
+            if (File.Exists("freepremium.txt"))
+                License = true;
+
+            Program.UserDataTemplate UserData = Program.UserData[UserID];
+
+            Program.UserData[UserID] = UserData;
+
+            async void CreateMessage()
+            {
+                var Embed = new EmbedBuilder();
+                IUserMessage IUserMessage = null;
+
+                Embed.WithAuthor((new EmbedAuthorBuilder { Name = "PSU Obfuscation" }));
+                Embed.WithFooter(F => F.Text = $"PSU Obfuscator {Program.VERSION}").WithColor(Color.DarkGrey).WithCurrentTimestamp();
+
+                if (License)
+                {
+                    Embed.WithDescription("Please upload a file or respond with formatted text to complete the process. **If you want this session to be private, please obfuscate in DMs!**");
+
+                    Embed.WithTitle("Obfuscation Settings");
+
+                    Embed.AddField(":regional_indicator_a: Encrypt All Strings", "Encrypts all strings in the file where they are defined. **This setting should NOT be used in large scripts and will cause a large performance loss and increase in obfuscation time.**");
+                    Embed.AddField(":regional_indicator_b: Disable Super Operators", "Disables generation of Super Operators. This may fix some errors in scripts, but will also decrease performance.");
+                    Embed.AddField(":regional_indicator_c: Maximum Security", "Uses more aggressive forms of security to prevent deobfuscation. This will GREATLY affect performance speed and is NOT reccommended on large scipts.");
+                    Embed.AddField(":regional_indicator_d: Enhanced Output", "Generates the ULTIMATE output. Makes file size greatly larger but improves security.");
+                    Embed.AddField(":regional_indicator_e: Premium Output", "Uses a differnt script structure than normal. Requires loadstring() or load(). This feature will also make PSU_MAX_SECURITY more secure against opcode hooking.");
+                    Embed.AddField("Vanity Bytecode", "Replaces the normal Base36 formatted bytecode with a custom skin. Type the mode you would like to use with the file you upload. **Available Modes: Chinese, Arabic, Korean, Emoji, Greek, Symbols1, Symbols2, Symbols3, Default (Base36)**");
+
+                    IUserMessage = await ReplyAsync($"{User.Mention}, Thank you for using PSU Obfuscation!", (false), Embed.Build());
+
+                    if (Program.Processes.ContainsKey(UserID)) { Program.StopObfuscationProcess(UserID); };
+                    Program.Processes[UserID] = (new Program.ObfuscationProcess { UserId = UserID, IUserMessage = IUserMessage, License = true });
+
+                    async void AddReactions() { await IUserMessage.AddReactionAsync(Program.A); await IUserMessage.AddReactionAsync(Program.B); await IUserMessage.AddReactionAsync(Program.C); await IUserMessage.AddReactionAsync(Program.D); await IUserMessage.AddReactionAsync(Program.E); };
+                    new Thread(AddReactions).Start();
+                }
+                else if (ServerBoost)
+                {
+                    Embed.WithDescription("Please upload a file or respond with formatted text to complete the process. **If you want this session to be private, please obfuscate in DMs!**");
+
+                    Embed.WithTitle("Obfuscation Settings");
+
+                    Embed.AddField(":regional_indicator_a: Encrypt All Strings", "Encrypts all strings in the file where they are defined. **This setting should NOT be used in large scripts and will cause a large performance loss and increase in obfuscation time.**");
+                    Embed.AddField(":regional_indicator_b: Disable Super Operators", "Disables generation of Super Operators. This may fix some errors in scripts, but will also decrease performance.");
+
+                    IUserMessage = await ReplyAsync($"{User.Mention}, Thank you for using PSU Obfuscation!", (false), Embed.Build());
+
+                    if (Program.Processes.ContainsKey(UserID)) { Program.StopObfuscationProcess(UserID); };
+                    Program.Processes[UserID] = (new Program.ObfuscationProcess { UserId = UserID, IUserMessage = IUserMessage, License = false, Boost = true });
+
+                    async void AddReactions() { await IUserMessage.AddReactionAsync(Program.A); await IUserMessage.AddReactionAsync(Program.B); };
+                    new Thread(AddReactions).Start();
+                }
+                else
+                {
+                    Embed.WithDescription("Please upload a file or respond with formatted text to complete the process. *If you want this session to be private, please obfuscate in DMs!*\n\n**You are using a non-licensed version of this product. Please upgrade to remove limitations and unlock all features: https://www.psu.dev/upgrade**");
+
+                    IUserMessage = await ReplyAsync($"{User.Mention}, Thank you for using PSU Obfuscation! You have **∞** obfuscations remaining today!", (false), Embed.Build());
+
+                    if (Program.Processes.ContainsKey(UserID)) { Program.StopObfuscationProcess(UserID); };
+                    Program.Processes[UserID] = (new Program.ObfuscationProcess { UserId = UserID, IUserMessage = IUserMessage, License = false });
+                };
+            };
+
+            new Thread(CreateMessage).Start();
+        }
+        [Command("enablerestricted")]
+        public async Task EnabledRestrictedMode()
+        {
+            var Embed = new EmbedBuilder();
+            IUserMessage IUserMessage = null;
+
+            Embed.WithAuthor((new EmbedAuthorBuilder { Name = "PSU Obfuscation" }));
+            Embed.WithFooter(F => F.Text = $"PSU Obfuscator {Program.VERSION}").WithColor(Color.Red).WithCurrentTimestamp();
+
+            Program.RESTRICTED_MODE = true;
+
+            Embed.WithDescription("RestrictedMode");
+
+            IUserMessage = await ReplyAsync($"Restricted mode: **Enabled**", (false), Embed.Build());
+        }
+        [Command("disablerestricted")]
+        public async Task DisabledRestrictedMode()
+        {
+            var Embed = new EmbedBuilder();
+            IUserMessage IUserMessage = null;
+
+            Embed.WithAuthor((new EmbedAuthorBuilder { Name = "PSU Obfuscation" }));
+            Embed.WithFooter(F => F.Text = $"PSU Obfuscator {Program.VERSION}").WithColor(Color.Green).WithCurrentTimestamp();
+
+            Program.RESTRICTED_MODE = false;
+
+            Embed.WithDescription("RestrictedMode");
+
+            IUserMessage = await ReplyAsync($"Restricted mode: **Disabled**", (false), Embed.Build());
+        }
+    };
+};
---
+++ bot-dotnet/Program.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Reflection;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.IO;
+
+using System.Text;
+using System.Text.RegularExpressions;
+
+using System.Collections;
+using System.Collections.Generic;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+using Microsoft;
+
+using Microsoft.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+using Obfuscator;
+using Obfuscator.Obfuscation;
+
+namespace DiscordBot
+{
+    public class Program
+    {
+        static void Main(string[] Arguments) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        public static ulong EPIC = 459569058526789632;
+        public static bool RESTRICTED_MODE = false;
+        public static string VERSION = "4.0.A";
+
+        public static DiscordSocketClient Client;
+        private static CommandService Commands;
+        private static IServiceProvider Services;
+
+        private static Encoding LuaEncoding = Encoding.GetEncoding(28591);
+
+        private static string Token = "OTI5NzU1NDUzNjI4NTAyMDE2.Ydr8Pg.7i80gfeg0UsBLh9uFo9IrBR6J9U";
+
+        public static Emoji A = new Emoji("🇦");
+        public static Emoji B = new Emoji("🇧");
+        public static Emoji C = new Emoji("🇨");
+        public static Emoji D = new Emoji("🇩");
+        public static Emoji E = new Emoji("🇪");
+        public static Emoji F = new Emoji("🇫");
+
+        public async Task RunBotAsync()
+        {
+            Client = new DiscordSocketClient();
+
+            Commands = new CommandService();
+
+            Services = new ServiceCollection().AddSingleton(Client).AddSingleton(Commands).BuildServiceProvider();
+
+            Client.Log += ClientLog;
+
+            await RegisterCommandsAsync();
+
+            await Client.LoginAsync(TokenType.Bot, Token);
+
+            await Client.StartAsync();
+
+            await Client.SetStatusAsync(UserStatus.Online);
+
+            await Task.Delay(-(1));
+        }
+
+        private Task ClientLog(LogMessage Argument) { return Task.CompletedTask; }
+        public async Task RegisterCommandsAsync() { Client.MessageReceived += HandleCommandAsync; await Commands.AddModulesAsync(Assembly.GetEntryAssembly(), Services); }
+
+        //////////////////////////////////////////////////
+
+        public struct ObfuscationProcess { public ulong UserId; public IUserMessage IUserMessage; public Obfuscator.Obfuscator Obfuscator; public bool Obfuscating; public Process Process; public bool License; public bool Boost; };
+        public struct UserDataTemplate { public long Time; public short Obfuscations; public long ObfuscationTime; };
+
+        public static Dictionary<ulong, ObfuscationProcess> Processes = new Dictionary<ulong, ObfuscationProcess>();
+        public static Dictionary<ulong, UserDataTemplate> UserData = new Dictionary<ulong, UserDataTemplate>();
+
+        //////////////////////////////////////////////////
+        
+        public static void StopObfuscationProcess(ulong UserId)
+        {
+            if (Processes.ContainsKey(UserId))
+            {
+                ObfuscationProcess ObfuscationProcess = Processes[UserId];
+                ObfuscationProcess.Obfuscating = false;
+
+                if (ObfuscationProcess.Obfuscator != null)
+                    ObfuscationProcess.Obfuscator.Obfuscating = false;
+
+                if (Processes[UserId].Process != null)
+                    Processes[UserId].Process.Kill();
+
+                Processes.Remove(UserId);
+            };
+        }
+
+        private async Task HandleCommandAsync(SocketMessage Argument)
+        {
+            if (!(Argument is SocketUserMessage)) return;
+            SocketUserMessage Message = (Argument as SocketUserMessage);
+
+            SocketCommandContext Context = new SocketCommandContext(Client, Message);
+
+            if ((object.ReferenceEquals(Message.Author, null)) || (Message.Author.IsBot)) { return; };
+
+            ulong UserId = Message.Author.Id;
+
+            if (RESTRICTED_MODE && UserId != EPIC) return;
+
+            int ArgumentPosition = 0;
+
+            if (Message.HasStringPrefix("!", ref ArgumentPosition))
+            {
+                if (!UserData.ContainsKey(UserId))
+                    UserData.Add(UserId, new UserDataTemplate { Time = 0 });
+
+                if ((DateTime.Now.Ticks - UserData[UserId].Time) < 10000000)
+                    return;
+
+                UserDataTemplate UserDataTemplate = UserData[UserId];
+                UserDataTemplate.Time = DateTime.Now.Ticks;
+
+                Commands.ExecuteAsync(Context, ArgumentPosition, Services);
+            }
+            else if (Processes.ContainsKey(UserId))
+            {
+                ObfuscationProcess ObfuscationProcess = Processes[UserId];
+
+                string VanityByteCodeMode = "Default";
+
+                if (ObfuscationProcess.License)
+                {
+                    VanityByteCodeMode = Message.Content;
+
+                    switch (Message.Content)
+                    {
+                        case ("Chinese"):
+                        case ("Arabic"):
+                        case ("Korean"):
+                        case ("Emoji"):
+                        case ("Greek"):
+                        case ("Symbols #1"):
+                        case ("Symbols #2"):
+                        case ("Symbols #3"):
+                            {
+                                break;
+                            };
+
+                        default: { VanityByteCodeMode = "Default"; break; };
+                    };
+                };
+
+                IUserMessage IUserMessage = ObfuscationProcess.IUserMessage;
+
+                if (ObfuscationProcess.Obfuscating) { return; };
+                ObfuscationProcess.Obfuscating = true;
+
+                async void StartObfuscationProcess()
+                {
+                    void Error(string Message) { var Embed = new EmbedBuilder { Description = $"Error While Obfuscating: ``{Message}``" }; Embed.WithFooter(F => F.Text = $"{VERSION}").WithColor(Color.DarkGrey).WithCurrentTimestamp(); IUserMessage.ModifyAsync(M => M.Embed = Embed.Build()); StopObfuscationProcess(UserId); };
+
+                    string Content = "";
+
+                    if (Message.Attachments.Count == 0)
+                    {
+                        if ((Message.Content.Length > 7) && (Message.Content.Substring(0, 7).ToLower() == "```lua\n") && (Message.Content.EndsWith("```")))
+                        {
+                            Content = Message.Content.Substring(7);
+                            Content = Content.Substring(0, Content.Length - 3);
+                        }
+                        else { Error("Invalid Response"); return; };
+                    }
+                    else if (Message.Attachments.Count > 0)
+                    {
+                        Attachment Attachment = Message.Attachments.First();
+
+                        WebClient WebClient = new WebClient();
+
+                        string URL = Attachment.Url;
+
+                        byte[] Buffer = WebClient.DownloadData(URL);
+
+                        Content = LuaEncoding.GetString(Buffer);
+                    };
+
+                    string Folder = $@"Storage\Obfuscation\{UserId.ToString()}";
+
+                    if (Directory.Exists(Folder)) { Directory.Delete(Folder, true); };
+                    Directory.CreateDirectory(Folder);
+
+                    try
+                    {
+                        var Embed = new EmbedBuilder();
+                        Embed.WithAuthor((new EmbedAuthorBuilder { Name = "Script Obfuscation" }));
+                        Embed.WithFooter(F => F.Text = $"{VERSION}").WithColor(Color.DarkGrey).WithCurrentTimestamp();
+
+                        string Input = Path.Combine(Folder, "Input.lua");
+
+                        File.WriteAllText(Input, Content, LuaEncoding);
+                        FileInfo FileInfo = new FileInfo(Input);
+
+                        IMessage IMessage = IUserMessage.Channel.GetMessageAsync(IUserMessage.Id).Result;
+
+                        ObfuscationSettings ObfuscationSettings;
+
+                        if (ObfuscationProcess.License || ObfuscationProcess.Boost)
+                        {
+                            ObfuscationSettings = new ObfuscationSettings
+                            {
+                                EncryptAllStrings = (IMessage.Reactions.ContainsKey(A) && IMessage.Reactions[A].ReactionCount > 1),
+                                DisableSuperOperators = (IMessage.Reactions.ContainsKey(B) && IMessage.Reactions[B].ReactionCount > 1),
+                                ControlFlowObfuscation = true,
+                                EnhancedConstantEncryption = false,
+                                ConstantEncryption = false,
+                                MaximumSecurityEnabled = (IMessage.Reactions.ContainsKey(C) && IMessage.Reactions[C].ReactionCount > 1),
+                                EnhancedOutput = (IMessage.Reactions.ContainsKey(D) && IMessage.Reactions[D].ReactionCount > 1),
+                                PremiumFormat = (IMessage.Reactions.ContainsKey(E) && IMessage.Reactions[E].ReactionCount > 1),
+                                ByteCodeMode = VanityByteCodeMode
+                            };
+                        }
+                        else
+                        {
+                            ObfuscationSettings = new ObfuscationSettings
+                            {
+                                EncryptAllStrings = false,
+                                DisableSuperOperators = true,
+                                ControlFlowObfuscation = false,
+                                ConstantEncryption = false,
+                                MaximumSecurityEnabled = false,
+                                DisableAllMacros = true,
+                                EnhancedOutput = false,
+                                PremiumFormat = false,
+                                ByteCodeMode = "Default"
+                            };
+
+                            if (FileInfo.Length > 250000) { Error("Non-Premium Users Cannot Obfuscate Files Larger Than 250Kb"); return; };
+                        };
+
+                        if (FileInfo.Length > 2500000) { Error("Cannot Obfuscate Files Larger Than 2.5Mb (Please Use The Web Panel or API For Large Files!)"); return; };
+
+                        Console.WriteLine($"Obfuscating File From {Message.Author.Username}#{Message.Author.Discriminator.ToString()} ({Message.Author.Id}) @ {Message.Timestamp} ({FileInfo.Length} Bytes)");
+
+                        if (!ObfuscationProcess.License)
+                        {
+                            Program.UserDataTemplate UserData = Program.UserData[UserId];
+                            UserData.Obfuscations += 1;
+                            Program.UserData[UserId] = UserData;
+                        };
+
+                        Stopwatch ElapsedTime = new Stopwatch();
+                        ElapsedTime.Start();
+
+                        bool Success = false;
+
+                        ObfuscationSettings.DebugMode = RESTRICTED_MODE;
+
+                        ObfuscationProcess.Obfuscator = new Obfuscator.Obfuscator(ObfuscationSettings, Folder);
+
+                        async void Obfuscate()
+                        {
+                            Stopwatch Stopwatch = new Stopwatch(); Stopwatch.Start();
+
+                            string ErrorMessage = "";
+
+                            Embed.AddField("Obfuscation Status", ":arrows_counterclockwise: Compiling...");
+                            await IUserMessage.ModifyAsync(M => M.Embed = Embed.Build());
+
+                            Stopwatch.Restart(); try { Success = ObfuscationProcess.Obfuscator.Compile(out ErrorMessage); Stopwatch.Stop(); } catch (Exception Exception) { Console.WriteLine(Exception); Error("Error While Obfuscating File"); }; if (!Success) { Error(ErrorMessage); return; };
+                            string CompileTime = ((float)Stopwatch.ElapsedMilliseconds / 1000).ToString();
+
+                            Embed.Fields.Clear();
+                            Embed.AddField("Obfuscation Status", $":ballot_box_with_check: Compiled in {CompileTime} Seconds\n:arrows_counterclockwise: Deserializing...");
+                            IUserMessage.ModifyAsync(M => M.Embed = Embed.Build());
+
+                            Stopwatch.Restart(); try { Success = ObfuscationProcess.Obfuscator.Deserialize(out ErrorMessage); Stopwatch.Stop(); } catch (Exception Exception) { Console.WriteLine(Exception); Error("Error While Obfuscating File"); }; if (!Success) { Error(ErrorMessage); return; };
+                            string DeserializeTime = ((float)Stopwatch.ElapsedMilliseconds / 1000).ToString();
+
+                            Embed.Fields.Clear();
+                            Embed.AddField("Obfuscation Status", $":ballot_box_with_check: Compiled in {CompileTime} Seconds\n:ballot_box_with_check: Deserialized in {DeserializeTime} Seconds\n:arrows_counterclockwise: Obfuscating...");
+                            IUserMessage.ModifyAsync(M => M.Embed = Embed.Build());
+
+                            Stopwatch.Restart(); try { Success = ObfuscationProcess.Obfuscator.Obfuscate(out ErrorMessage); } catch (Exception Exception) { Console.WriteLine(Exception); Error("Error While Obfuscating File"); }; Stopwatch.Stop(); if (!Success) { Error(ErrorMessage); return; };
+                            string ObfuscateTime = ((float)Stopwatch.ElapsedMilliseconds / 1000).ToString();
+
+                            Embed.Fields.Clear();
+                            Embed.AddField("Obfuscation Status", $":ballot_box_with_check: Compiled in {CompileTime} Seconds\n:ballot_box_with_check: Deserialized in {DeserializeTime} Seconds\n:ballot_box_with_check: Obfuscated in {ObfuscateTime} Seconds");
+                            IUserMessage.ModifyAsync(M => M.Embed = Embed.Build());
+
+                            string TimeTaken = ((float)ElapsedTime.ElapsedMilliseconds / 1000).ToString();
+
+                            try { await IMessage.Channel.SendFileAsync(Path.Combine(Folder, "Output.lua"), $"{Message.Author.Mention}, Obfuscated in {TimeTaken} Seconds!"); } catch (Exception Exception) { Console.WriteLine(Exception); Error("Error While Uploading File"); return; };
+
+                            ElapsedTime.Stop();
+
+                            Directory.Delete(Folder, true);
+                        }
+
+                        Thread Thread = new Thread(Obfuscate); Thread.Start();
+                        if (!Thread.Join(60000)) { try { Thread.Abort(); Error("Obfuscation Timeout"); } catch { }; };
+
+                        ObfuscationProcess.Obfuscating = false;
+                        StopObfuscationProcess(UserId);
+                        if (Processes.ContainsKey(UserId)) { Processes.Remove(UserId); };
+                    }
+                    catch (Exception Exception) { Console.WriteLine(Exception); Error("Unknown Error #2"); StopObfuscationProcess(UserId); return; };
+                }
+
+                new Thread(StartObfuscationProcess).Start();
+            };
+        }
+    };
+};
